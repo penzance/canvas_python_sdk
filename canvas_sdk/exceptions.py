c54309df75ce6c20cbe38ed000242d4151cd925b
Bermudez, Jaime <jaime_bermudez@harvard.edu>
2014-11-12 16:32:54 -0500

    Add .sublime-project settings file to project per Sublime doc recommendations.  Ignore the .sublime-workspace file that might be there as a result of using a project.  Add __unicode__ method to CanvasAPIError class definition and add tests for this class.

--- canvas_sdk/exceptions.py
+++ canvas_sdk/exceptions.py
@@ -14,6 +14,15 @@ class CanvasAPIError(SDKException):
         self.status_code = status_code
         self.error_msg = msg
         self.error_json = error_json
-
+    
+    """
+    NOTE: this method of fully defining unicode and having str call upon it is referenced
+    in official pydocs (https://docs.python.org/2/howto/pyporting.html) and other porting
+    libraries (like Django utils).  If we move to Python 3, we'd rewrite this to just have
+    a __str__ method and use futures and mixins to play nice with Python 2.
+    """
     def __str__(self):
-        return '%s: %s' % (self.status_code, self.error_msg)
+        return self.__unicode__().encode('utf-8')
+
+    def __unicode__(self):
+        return u'%s: %s' % (self.status_code, self.error_msg)
---
+++ tests/test_exceptions.py
@@ -0,0 +1,38 @@
+import unittest
+from canvas_sdk.exceptions import CanvasAPIError
+
+
+class TestExceptions(unittest.TestCase):
+    longMessage = True
+
+    def setUp(self):
+        self.default_api_error = CanvasAPIError()
+
+    def test_default_status_for_canvas_api_error(self):
+        """ Test expected default status for instance of CanvasAPIError """
+        self.assertEqual(self.default_api_error.status_code, 500)
+
+    def test_default_message_for_canvas_api_error(self):
+        """ Test expected default msg attribute for instance of CanvasAPIError """
+        self.assertIsNone(self.default_api_error.error_msg)
+
+    def test_default_error_json_for_canvas_api_error(self):
+        """ Test expected default error_json attribute for instance of CanvasAPIError """
+        self.assertIsNone(self.default_api_error.error_json)
+
+    def test_default_str_for_canvas_api_error(self):
+        """ Test default CanvasAPIError instance represented as a str """
+        self.assertEqual('500: None', str(self.default_api_error))
+
+    def test_default_unicode_for_canvas_api_error(self):
+        """ Test default CanvasAPIError instance represented as unicode """
+        self.assertEqual(u'500: None', unicode(self.default_api_error))
+
+    def test_instance_str_for_canvas_api_error(self):
+        """ Test string representation of CanvasAPIError with custom attributes """
+        status = 404
+        error_msg = 'This is a test message'
+        error_json = {'Some error json'}
+
+        api_error = CanvasAPIError(status_code=status, msg=error_msg, error_json=error_json)
+        self.assertEqual('%d: %s' % (status, error_msg), str(api_error))
