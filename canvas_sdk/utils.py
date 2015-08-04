c4f7d5e6bdf176ff33a69070353890a2b1171360
Douglas Hall <douglas_hall@harvard.edu>
2015-08-04 20:37:13 +0000

    Allow for params to be a list or tuple

--- canvas_sdk/utils.py
+++ canvas_sdk/utils.py
@@ -1,3 +1,5 @@
+import collections
+
 from canvas_sdk import client
 from collections import defaultdict
 
@@ -5,18 +7,22 @@ from collections import defaultdict
 The util module contains helper methods for the SDK
 """
 
+
 def validate_attr_is_acceptable(value, acceptable_values=[], allow_none=True):
     """
     Test an input value against a list of acceptable values.  A value of None may or may
     not be considered valid.  If the input is not valid, an Attribute error is raised, otherwise
     nothing is returned.
     """
-    if value not in acceptable_values:
-        # We know the value is not one of the acceptable values, but we need to also make sure
-        # that the value is not None and that None is not an allowable value before raising
-        # an exception
-        if value is not None or not allow_none:
-            raise AttributeError("%s must be one of %s" % (value, acceptable_values))
+    if type(value) not in (list, tuple):
+        value = [value]
+    for v in value:
+        if v not in acceptable_values:
+            # We know the value is not one of the acceptable values, but we need to also make sure
+            # that the value is not None and that None is not an allowable value before raising
+            # an exception
+            if v is not None or not allow_none:
+                raise AttributeError("%s must be one of %s" % (v, acceptable_values))
 
 
 def get_next(request_context, response):
