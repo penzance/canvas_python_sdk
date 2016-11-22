00cb582151dca527021b33e0baffc5e7a7a60faf
Yates, Elliott <elliott_yates@harvard.edu>
2016-11-22 14:28:04 -0500

    tlt-2802: users/list_users_in_account - allow include param (override -- undocumented)

--- canvas_sdk/methods/users.py
+++ canvas_sdk/methods/users.py
@@ -1,15 +1,31 @@
 from canvas_sdk import client, utils
 
-def list_users_in_account(request_ctx, account_id, search_term=None, per_page=None, **request_kwargs):
+
+def list_users_in_account(request_ctx, account_id, search_term=None,
+                          include=None, per_page=None, **request_kwargs):
     """
     Retrieve the list of users associated with this account.
+
+     @example_request
+       curl https://<canvas>/api/v1/accounts/self/users?search_term=<search value> \
+          -X GET \
+          -H 'Authorization: Bearer <token>'
 
         :param request_ctx: The request context
         :type request_ctx: :class:RequestContext
         :param account_id: (required) ID
         :type account_id: string
-        :param search_term: (optional) The partial name or full ID of the users to match and return in the results list. Must be at least 3 characters.
+        :param search_term: (optional) The partial name or full ID of the users to match and return in the
+results list. Must be at least 3 characters.
+
+Note that the API will prefer matching on canonical user ID if the ID has
+a numeric form. It will only search against other fields if non-numeric
+in form, or if the numeric value doesn't yield any matches. Queries by
+administrative users will search on SIS ID, name, or email address; non-
+administrative queries will only be compared against name.
         :type search_term: string or None
+        :param include: (optional) One of (avatar_url, email, last_login, time_zone)
+        :type include: array or None
         :param per_page: (optional) Set how many results canvas should return, defaults to config.LIMIT_PER_PAGE
         :type per_page: integer or None
         :return: List users in account
@@ -19,10 +35,13 @@ def list_users_in_account(request_ctx, account_id, search_term=None, per_page=No
 
     if per_page is None:
         per_page = request_ctx.per_page
+    include_types = ('avatar_url', 'email', 'last_login', 'time_zone')
+    utils.validate_attr_is_acceptable(include, include_types)
     path = '/v1/accounts/{account_id}/users'
     payload = {
-        'search_term' : search_term,
-        'per_page' : per_page,
+        'include[]': include,
+        'search_term': search_term,
+        'per_page': per_page,
     }
     url = request_ctx.base_api_url + path.format(account_id=account_id)
     response = client.get(request_ctx, url, payload=payload, **request_kwargs)
